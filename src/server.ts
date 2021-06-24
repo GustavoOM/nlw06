ed3e58969c33475c3a41e9443e0bf2e0aea457ed
GustavoOM <gumartins2001@gmail.com>
2021-06-24 16:56:13 -0300

    Create tags

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
+import express, { Request, Response, NextFunction, response } from "express";
 import "reflect-metadata";
-import express, { request, response } from "express"
-import {router} from "./routes"
 import "./database";
-const port = 3000
-const app = express();
+import "express-async-errors";
 
-app.use(express.json())
+import { router } from "./routes";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
 app.use(router)
-app.listen(port, () => console.log(`🍧 Server is running on port ${port}!`))
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+}) 
+
+
+// http://localhost:3000
+app.listen(port, () => console.log(`🍧 Server is running. Port ${port}`) )
